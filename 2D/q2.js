c6b8d3d19fa20aa20565dce1ae3a3ddddb816c16
Ananthika <ananthikaspillai@gmail.com>
2023-07-19 12:00:31 +0530

    update

--- 2D/q2.js
+++ 2D/q2.js
@@ -1,11 +1,7 @@
 let arr = [100, 20, 31, 150, 39, 72]
-
 let min = arr[0]
-
 let max = arr[0]
-
 for(let i=0; i<arr.length;i++)
-
 if(arr[i]>max){
 max = arr[i]
 }
--- 2d array/q5.js
+++
@@ -1,9 +0,0 @@
-const arr=['assignment','problem','media','upload'];
-var b=[];
-for(let i=0;i<arr.length;i++)
-    {
-    var j=arr[i].length-1;
-    if(arr[i][0]=='a' || arr[i][j]=='a')
-        b.push(arr[i]);
-    }
-console.log(b);
